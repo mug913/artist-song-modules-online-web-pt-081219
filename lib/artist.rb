59d773e6c249ac12376312a2e1507613badbf63f
Brad Alexander <brad.alexander913@gmail.com>
2019-09-19 20:15:56 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,8 +4,8 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
-  extend Memorable, Findable
-  include Paramable
+  extend Memorable::ClassMethods, Findable
+  include Paramable, Memorable::InstanceMethods
   
   @@artists = []
 
